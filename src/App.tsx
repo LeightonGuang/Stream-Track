8953c7b10bf07f116d815eebfa0568149b125d08
Leighton Guang <leighton.guang@gmail.com>
2023-12-03 04:32:15 +0000

    style: setup file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import Header from "./components/Header/Header";
