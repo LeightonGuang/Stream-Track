574cff12f7796bf43ec44751a24500e522912e24
Leighton Guang <leighton.guang@gmail.com>
2023-12-03 05:28:10 +0000

    feat: nav link to their own pages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+import "./Header.scss";
+
 export default function Header() {
   return (
     <div className="header">
-      <h1>Stream Track</h1>
+      <div className="header__container">
+        <h1 className="header__title">Stream Track</h1>
+        <nav className="header__nav">
+          <ul className="header__list">
+            <Link to={"/"} className="header__link header__link--all">
+              All
+            </Link>
+            <Link to={"/twitch"} className="header__link header__link--twitch">
+              Twitch
+            </Link>
+            <Link
+              to={"/youtube"}
+              className="header__link header__link--youtube"
+            >
+              Youtube
+            </Link>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
