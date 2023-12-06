b908bd2bbd6102c307dc73e8971d2d8b045f586c
Leighton Guang <leighton.guang@gmail.com>
2023-12-06 18:53:51 +0000

    feat: add routes for different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
 import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
+import SettingPage from "./pages/SettingPage/SettingPage";
 
 export default function App() {
   return (
@@ -9,6 +10,9 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/twitch" element={<HomePage />} />
+          <Route path="/youtube" element={<HomePage />} />
+          <Route path="/setting" element={<SettingPage />} />
         </Routes>
       </BrowserRouter>
     </div>
