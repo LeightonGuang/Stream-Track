2a7273cf3bab92716f50d2584481369b11074088
Leighton Guang <leighton.guang@gmail.com>
2023-12-06 19:43:02 +0000

    feat: add a setting link to setting page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.scss";
 
 export default function Header() {
   return (
     <div className="header">
       <div className="header__container">
-        <h1 className="header__title">Stream Track</h1>
+        <div className="header__title-container">
+          <h1 className="header__title">Stream Track</h1>
+          <Link to={"/setting"} className="header__title--setting">
+            Setting
+          </Link>
+        </div>
         <nav className="header__nav">
           <ul className="header__list">
             <NavLink to={"/"} className="header__link header__link--all">
