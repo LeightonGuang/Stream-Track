2c83d914eec6a976bb3ba8f844232009e0b33a67
LeightonGuang <leighton.guang@icloud.com>
2025-03-19 09:54:55 +0000

    Feat: New popup design

--- src/chrome-extension/popup/index.tsx
+++ src/chrome-extension/popup/index.tsx
@@ -1,9 +1,45 @@
 import "../global.css";
+import { useState } from "react";
+import ListTab from "../components/ListTab";
+import SettingsTab from "../components/settingsTab";
+
+import { ListIcon, SettingsIcon } from "../public/icons";
 
 export const Popup = () => {
+  const [selectedTab, setSelectedTab] = useState<string>("list");
+
   return (
-    <div className="text-5xl p-10 font-extrabold">
-      <div>This is your popup.</div>
+    <div className="bg-background text-white">
+      <div className="bg-twitch py-2 text-center text-xl font-bold text-white">
+        <h1>Stream Track</h1>
+      </div>
+
+      <div className="flex w-full gap-2 p-2">
+        <button
+          className={`flex w-full items-center justify-center gap-2 rounded-md px-4 py-2 text-xs font-semibold hover:bg-[#231628] hover:text-[#d0c8d4] ${selectedTab === "list" ? "bg-[#391e47]" : "bg-[#1e1623] text-[#847e88]"}`}
+          onClick={() => {
+            setSelectedTab("list");
+          }}
+        >
+          <ListIcon className="w-4" />
+          List
+        </button>
+
+        <button
+          className={`flex w-full items-center justify-center gap-2 rounded-md px-4 py-2 text-xs font-semibold hover:bg-[#231628] hover:text-[#d0c8d4] ${selectedTab === "settings" ? "bg-[#391e47]" : "bg-[#1e1623] text-[#847e88]"}`}
+          onClick={() => {
+            setSelectedTab("settings");
+          }}
+        >
+          <SettingsIcon className="w-4" />
+          Settings
+        </button>
+      </div>
+
+      <div className="p-2">
+        {selectedTab === "list" && <ListTab />}
+        {selectedTab === "settings" && <SettingsTab />}
+      </div>
     </div>
   );
 };
