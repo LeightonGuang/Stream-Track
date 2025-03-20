61c2e45647777e8c928c36175600e5092590ebc4
LeightonGuang <leighton.guang@icloud.com>
2025-03-20 06:21:11 +0000

    Fix: Hide decimal place when it's 0

--- src/chrome-extension/utils/formatViewCount.ts
+++ src/chrome-extension/utils/formatViewCount.ts
@@ -1,8 +1,12 @@
 const formatViewCount = (viewCount: number) => {
   if (viewCount >= 1000000) {
-    return `${(viewCount / 1000000).toFixed(1)}M`;
+    const value = (viewCount / 1000000).toFixed(1);
+    const [integerPart, decimalPart] = value.split(".");
+    return `${integerPart}${decimalPart !== "0" ? "." + decimalPart : ""}M`;
   } else if (viewCount >= 1000) {
-    return `${(viewCount / 1000).toFixed(1)}K`;
+    const value = (viewCount / 1000).toFixed(1);
+    const [integerPart, decimalPart] = value.split(".");
+    return `${integerPart}${decimalPart !== "0" ? "." + decimalPart : ""}K`;
   } else {
     return viewCount.toString();
   }
