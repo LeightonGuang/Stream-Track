507e686e21808c3da9b6d4d4233497a9224858e6
LeightonGuang <leighton.guang@icloud.com>
2025-03-25 15:04:56 +0000

    Refactor: Rename ListTab to FollowingTab

---
+++ src/chrome-extension/components/FollowingTab/index.tsx
@@ -0,0 +1,105 @@
+import StreamerCard from "../StreamerCard";
+import { useState, useEffect } from "react";
+import getStreamersLive from "../../utils/twitchApi/getStreamersLive";
+import getAppAccessToken from "../../utils/twitchApi/getAppAccessToken";
+import validateTwitchToken from "../../utils/twitchApi/validateTwitchToken";
+
+import { LiveChannelType } from "../../../types/liveChannelType";
+import { ChromeStorageFollowedChannelsType } from "../../../types/ChromeStorageFollowedChannelsType";
+
+const FollowingTab = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  // followedChannels is for streamers profile pictures
+  const [followedChannels, setFollowedChannels] = useState<
+    ChromeStorageFollowedChannelsType[]
+  >([]);
+  const [liveChannels, setLiveChannels] = useState<any[]>([]);
+
+  const processTasks = async () => {
+    try {
+      let { accessToken } = await chrome.storage.local.get("accessToken");
+
+      if (accessToken) {
+        const tokenIsValid = await validateTwitchToken(accessToken);
+
+        if (!tokenIsValid) {
+          console.error("Token is invalid");
+          accessToken = await getAppAccessToken();
+          await chrome.storage.local.set({ accessToken });
+        }
+      } else if (!accessToken) {
+        console.log("Access token is null");
+        accessToken = await getAppAccessToken();
+        await chrome.storage.local.set({ accessToken });
+      }
+
+      if (followedChannels.length === 0) return;
+      const live = await getStreamersLive(followedChannels, accessToken);
+      await chrome.storage.local.set({ liveChannels: live });
+      chrome.action.setBadgeBackgroundColor({ color: "#9146FF" });
+      chrome.action.setBadgeText({ text: live ? live.length.toString() : "0" });
+      console.log("streamers live: ", live);
+      setLiveChannels(live ?? []);
+    } catch (error) {
+      console.error("Error running processTasks: ", error);
+    }
+  };
+
+  useEffect(() => {
+    const getLocalLiveChannels = async () => {
+      const { liveChannels } = await chrome.storage.local.get("liveChannels");
+      if (liveChannels) {
+        console.log(liveChannels);
+        setLiveChannels(liveChannels);
+      } else {
+        setLiveChannels([]);
+      }
+    };
+
+    getLocalLiveChannels();
+  }, []);
+
+  useEffect(() => {
+    const getFollowedChannels = async () => {
+      const { followedChannels } =
+        await chrome.storage.local.get("followedChannels");
+      if (followedChannels) {
+        console.log(followedChannels);
+        setFollowedChannels(followedChannels);
+      } else {
+        setFollowedChannels([]);
+      }
+    };
+
+    getFollowedChannels();
+  }, []);
+
+  useEffect(() => {
+    processTasks();
+    setIsLoading(false);
+  }, [followedChannels]);
+
+  return (
+    <div className="flex h-min max-h-[calc(2.65rem*10)] flex-col items-center overflow-y-auto">
+      {!isLoading
+        ? liveChannels.map((liveChannelData: LiveChannelType) => {
+            const channelData = followedChannels.find(
+              (channel) =>
+                String(channel.id).trim() ===
+                String(liveChannelData.user_id).trim(),
+            );
+
+            return (
+              <StreamerCard
+                key={liveChannelData.user_id}
+                liveChannelData={liveChannelData}
+                channelData={channelData}
+              />
+            );
+          })
+        : "Loading..."}
+    </div>
+  );
+};
+
+export default FollowingTab;
