8c7ca2c9656a9b81e44e5d673dff38876e21dcd4
LeightonGuang <leighton.guang@icloud.com>
2025-03-25 17:34:14 +0000

    Feat: Change popup design

--- src/chrome-extension/popup/index.tsx
+++ src/chrome-extension/popup/index.tsx
@@ -1,19 +1,14 @@
 import "../global.css";
 import { useState } from "react";
-import ListTab from "../components/ListTab";
 import SettingsTab from "../components/settingsTab";
+import FollowingTab from "../components/FollowingTab";
 
-import { ListIcon, SettingsIcon } from "../public/icons";
-
+// TODO : Fix popup width and height to be responsive
 export const Popup = () => {
   const [selectedTab, setSelectedTab] = useState<string>("list");
 
   return (
     <div className="flex flex-col items-center whitespace-nowrap bg-background text-white">
-      <div className="w-full bg-twitch py-2 text-center text-xl font-bold text-white">
-        <h1>Stream Track</h1>
-      </div>
-
       <div className="flex h-[3.125rem] w-full bg-[#18181a]">
         <button
           className={`flex w-full items-center justify-center gap-2 border-b-[0.125rem] border-[#18181a] px-[0.625rem] py-2 text-sm font-semibold hover:text-[#bc98f1] ${selectedTab === "list" ? "border-[#bc98f1] text-[#bc98f1]" : "text-[##ececec]"}`}
@@ -21,8 +16,7 @@ export const Popup = () => {
             setSelectedTab("list");
           }}
         >
-          <ListIcon className="w-4" />
-          List
+          Following
         </button>
 
         <button
@@ -31,13 +25,12 @@ export const Popup = () => {
             setSelectedTab("settings");
           }}
         >
-          <SettingsIcon className="w-4" />
           Settings
         </button>
       </div>
 
-      <div className="flex h-max w-max justify-center">
-        {selectedTab === "list" && <ListTab />}
+      <div className="h-full bg-background">
+        {selectedTab === "list" && <FollowingTab />}
         {selectedTab === "settings" && <SettingsTab />}
       </div>
     </div>
