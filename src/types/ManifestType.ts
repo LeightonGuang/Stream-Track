f7e5f18e33ec2dce8539f37ec560850295cccf47
LeightonGuang <leighton.guang@icloud.com>
2025-03-25 05:28:31 +0000

    Feat: Update Manifest type

--- src/types/ManifestType.ts
+++ src/types/ManifestType.ts
@@ -1,8 +1,22 @@
 export interface ManifestType {
+  manifest_version: number;
+  name: string;
+  version: string;
+  action?: {
+    default_popup?: string;
+  };
+  background?: {
+    service_worker: string;
+    type: "module";
+  };
+  options_page?: string;
+  description: string;
+  icons: {
+    [key: string]: string;
+  };
   oauth2?: {
     client_id: string;
     scopes: string[];
-    redirect_uri: string;
   };
+  permissions: string[];
 }
- 
