6bfe52c310defa6103c81dcbe47d24289aa5f204
LeightonGuang <leighton.guang@icloud.com>
2025-03-26 11:00:41 +0000

    Fix: Height limit and background colour

--- src/optionsLocal.tsx
+++ src/optionsLocal.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
+import "./chrome-extension/global.css";
 import { createRoot } from "react-dom/client";
 import Options from "./chrome-extension/options/index";
-import "./chrome-extension/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="bg-white w-full h-[1000px]">
+    <div className="h-dvh w-dvw bg-background text-white">
       <Options />
     </div>
-  </StrictMode>
+  </StrictMode>,
 );
