a985e12c42845ec8cf32d8d54052b99d09c41b47
Leighton Guang <leighton.guang@gmail.com>
2025-03-27 09:45:57 +0000

    Feat: Add try-catch to async function

--- src/chrome-extension/utils/twitchApi/getAppAccessToken.ts
+++ src/chrome-extension/utils/twitchApi/getAppAccessToken.ts
@@ -6,31 +6,40 @@ const manifest = chrome.runtime.getManifest() as ManifestType;
 const clientId = manifest.oauth2?.client_id;
 
 const getAppAccessToken = async (): Promise<string | null> => {
-  return new Promise((resolve, reject) => {
-    const redirectUri = chrome.identity.getRedirectURL();
-    const authUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token&scope=user:read:follows&state=${generateRandomState()}`;
+  try {
+    console.log("getting access token");
+    return new Promise((resolve, reject) => {
+      const redirectUri = chrome.identity.getRedirectURL();
+      const authUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token&scope=user:read:follows&state=${generateRandomState()}`;
 
-    chrome.identity.launchWebAuthFlow(
-      { url: authUrl, interactive: true },
-      (redirectUrl) => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
-          reject(chrome.runtime.lastError.message);
-          return;
-        }
+      chrome.identity.launchWebAuthFlow(
+        { url: authUrl, interactive: true },
+        (redirectUrl) => {
+          if (chrome.runtime.lastError) {
+            console.error(
+              "Error launching web auth flow: ",
+              chrome.runtime.lastError.message,
+            );
+            reject(chrome.runtime.lastError.message);
+            return;
+          }
 
-        if (redirectUrl) {
-          const params = new URLSearchParams(
-            new URL(redirectUrl).hash.substring(1),
-          );
-          const accessToken = params.get("access_token");
-          resolve(accessToken || null);
-        } else {
-          reject("No redirect URL found");
-        }
-      },
-    );
-  });
+          if (redirectUrl) {
+            const params = new URLSearchParams(
+              new URL(redirectUrl).hash.substring(1),
+            );
+            const accessToken = params.get("access_token");
+            resolve(accessToken || null);
+          } else {
+            reject("No redirect URL found");
+          }
+        },
+      );
+    });
+  } catch (error) {
+    console.error("Error getting app access token: ", error);
+    return null;
+  }
 };
 
 export default getAppAccessToken;
