fa12c5cc164b68baf22dcd6d4a68b33dd79f5c95
Leighton Guang <leighton.guang@gmail.com>
2025-03-27 11:38:08 +0000

    Refactor: Move sort button to FollowingTab

--- src/chrome-extension/popup/index.tsx
+++ src/chrome-extension/popup/index.tsx
@@ -2,17 +2,12 @@ import "../global.css";
 import { useState } from "react";
 import SettingsTab from "../components/settingsTab";
 import FollowingTab from "../components/FollowingTab";
-import { SortIcon } from "../public/icons";
 
 // TODO : Fix popup width and height to be responsive
 // TODO : Sort by view count, name and game
 
 export const Popup = () => {
   const [selectedTab, setSelectedTab] = useState<string>("list");
-
-  const handleSortButton = () => {
-    console.log("Sort button clicked");
-  };
 
   return (
     <div className="flex flex-col items-center whitespace-nowrap bg-background text-white">
@@ -33,26 +28,6 @@ export const Popup = () => {
           }}
         >
           Settings
-        </button>
-      </div>
-
-      <div className="w-full">
-        <button
-          className="flex w-full items-center justify-between bg-[#1f1e22] hover:bg-[#302f35]"
-          onClick={handleSortButton}
-        >
-          <div className="m-[0.625rem] flex flex-col items-start">
-            <h3 className="text-[0.8125rem] font-semibold leading-[0.975rem] text-[#EFEFF1]">
-              FOLLOWED CHANNELS
-            </h3>
-            <p className="text-[0.8125rem] font-normal leading-[1.2188rem] text-[#ADADB8]">
-              Viewers (High to Low)
-            </p>
-          </div>
-
-          <div className="mr-[0.625rem] h-[1.25rem] w-[1.25rem] pr-[0.3125rem]">
-            <SortIcon className="h-[1.25rem] w-[1.25rem] text-white" />
-          </div>
         </button>
       </div>
 
