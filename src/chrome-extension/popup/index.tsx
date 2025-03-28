1fb446220e6c107dd9f50e934aafacf31e23eb22
LeightonGuang <leighton.guang@icloud.com>
2025-03-28 06:44:29 +0000

    Feat: Add purple twitch icon that links to the home page

--- src/chrome-extension/popup/index.tsx
+++ src/chrome-extension/popup/index.tsx
@@ -2,30 +2,41 @@ import "../global.css";
 import { useState } from "react";
 import SettingsTab from "../components/settingsTab";
 import FollowingTab from "../components/FollowingTab";
+import { TwitchPurpleIcon } from "../public/icons";
 
 export const Popup = () => {
   const [selectedTab, setSelectedTab] = useState<string>("list");
 
   return (
     <div className="flex flex-col items-center whitespace-nowrap bg-background text-white">
-      <div className="flex h-[3.125rem] w-full bg-[#18181a]">
-        <button
-          className={`flex w-full items-center justify-center gap-2 border-b-[0.125rem] border-[#18181a] px-[0.625rem] py-2 text-sm font-semibold hover:text-[#bc98f1] ${selectedTab === "list" ? "border-[#bc98f1] text-[#bc98f1]" : "text-[##ececec]"}`}
-          onClick={() => {
-            setSelectedTab("list");
-          }}
+      <div className="flex h-[3.125rem] w-full items-center bg-[#18181a]">
+        <a
+          className="ml-[0.625rem] flex h-10 w-10 min-w-10 items-center justify-center p-[0.3125rem]"
+          href="https://twitch.tv"
+          target="_blank"
         >
-          Following
-        </button>
+          <TwitchPurpleIcon className="h-7 w-6" />
+        </a>
 
-        <button
-          className={`flex w-full items-center justify-center gap-2 border-b-[0.125rem] border-[#18181a] px-[0.625rem] py-2 text-sm font-semibold hover:text-[#bc98f1] ${selectedTab === "settings" ? "border-[#bc98f1] text-[#bc98f1]" : "text-[##ececec]"}`}
-          onClick={() => {
-            setSelectedTab("settings");
-          }}
-        >
-          Settings
-        </button>
+        <div className="flex h-full w-full items-center justify-around">
+          <button
+            className={`h-full border-b-[0.125rem] border-[#18181a] text-sm font-semibold hover:text-[#bc98f1] ${selectedTab === "list" ? "border-[#bc98f1] text-[#bc98f1]" : "text-[##ececec]"}`}
+            onClick={() => {
+              setSelectedTab("list");
+            }}
+          >
+            Following
+          </button>
+
+          <button
+            className={`h-full border-b-[0.125rem] border-[#18181a] text-sm font-semibold hover:text-[#bc98f1] ${selectedTab === "settings" ? "border-[#bc98f1] text-[#bc98f1]" : "text-[##ececec]"}`}
+            onClick={() => {
+              setSelectedTab("settings");
+            }}
+          >
+            Settings
+          </button>
+        </div>
       </div>
 
       <div className="h-full bg-background">
