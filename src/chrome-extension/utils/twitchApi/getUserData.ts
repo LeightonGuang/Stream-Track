b3080d9297d709ec5f9521103e53995aa5ff70ce
LeightonGuang <leighton.guang@icloud.com>
2025-04-02 20:51:20 +0100

    Refactor: Convert getUserId to getUserData

--- src/chrome-extension/utils/twitchApi/getUserData.ts
+++ src/chrome-extension/utils/twitchApi/getUserData.ts
@@ -2,10 +2,12 @@ import axios from "axios";
 
 import { TwitchUserType } from "../../../types/twitchUserType";
 
-const getUserId = async (
+const getUserData = async (
   clientId: string | undefined,
   accessToken: string,
-): Promise<string | undefined> => {
+): Promise<
+  { id: string; profile_image_url: string; name: string } | undefined
+> => {
   try {
     const response: { data: { data: TwitchUserType[] } } = await axios.get(
       "https://api.twitch.tv/helix/users",
@@ -18,10 +20,12 @@ const getUserId = async (
     );
 
     const userId: string = response.data.data[0].id;
-    return userId;
+    const profile_image_url: string = response.data.data[0].profile_image_url;
+    const name = response.data.data[0].display_name;
+    return { id: userId, profile_image_url, name };
   } catch (error) {
     console.error("Error fetching user ID:", error);
   }
 };
 
-export default getUserId;
+export default getUserData;
