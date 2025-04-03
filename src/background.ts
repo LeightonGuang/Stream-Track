22630c81539af76c4234609761a6511fa54a9822
LeightonGuang <leighton.guang@icloud.com>
2025-04-03 18:06:02 +0100

    Fix: Background script to get user access token discreetly if token is invalid or missing

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,5 @@
-import getAppAccessToken from "./chrome-extension/utils/twitchApi/getAppAccessToken";
 import getStreamersLive from "./chrome-extension/utils/twitchApi/getStreamersLive";
+import getUserAccessToken from "./chrome-extension/utils/twitchApi/getUserAccessToken";
 import validateTwitchToken from "./chrome-extension/utils/twitchApi/validateTwitchToken";
 
 const fetchLiveFlow = async () => {
@@ -7,27 +7,45 @@ const fetchLiveFlow = async () => {
     const { followedChannels } =
       await chrome.storage.local.get("followedChannels");
 
+    if (!followedChannels) {
+      console.log("No followed channels found");
+      return;
+    }
+    console.log("followedChannels: ", followedChannels);
+
     let { accessToken } = await chrome.storage.local.get("accessToken");
+    console.log("accessToken local: ", accessToken);
 
     if (accessToken) {
       const tokenIsValid = await validateTwitchToken(accessToken);
-
       if (!tokenIsValid) {
-        console.error("Token is invalid");
-        return;
+        console.log("Token is invalid");
+        accessToken = await getUserAccessToken(false);
+        if (accessToken) {
+          console.log("Access token successfully fetched");
+          await chrome.storage.local.set({ accessToken });
+        } else {
+          throw new Error("Failed to fetch access token");
+        }
       }
     } else if (!accessToken) {
-      console.log("Access token is null");
-      accessToken = await getAppAccessToken();
-      await chrome.storage.local.set({ accessToken });
+      console.log("No access token found locally");
+      accessToken = await getUserAccessToken(false);
+      if (accessToken) {
+        console.log("Access token successfully fetched");
+        await chrome.storage.local.set({ accessToken });
+      } else {
+        throw new Error("Failed to fetch access token");
+      }
     }
 
     const channelsLive = await getStreamersLive(followedChannels, accessToken);
+    console.log("channelsLive: ", channelsLive);
     await chrome.storage.local.set({ liveChannels: channelsLive });
     chrome.action.setBadgeBackgroundColor({ color: "#9146FF" });
     chrome.action.setBadgeText({ text: channelsLive.length.toString() });
   } catch (error) {
-    console.error("Error fetching live flow:", error);
+    console.error("Error: ", error);
   }
 };
 
